de8dd515fce0d9a4fc8351dd37381880652341be
Cristina Lester <cristina.deelester@gmail.com>
2022-03-07 14:47:24 -0800

    added styles for review lists

--- src/Components/Product.js
+++ src/Components/Product.js
@@ -10,48 +10,50 @@ function Product(props) {
   const { name, imgUrl, avgRating, reviews } = location.state;
 
   return (
-    <Box sx={{ flexGrow: 1 }}>
-      <Grid container spacing={2}>
-        <Grid item xs={6}>
-          <img src={imgUrl} alt={name} />
-        </Grid>
-        <Grid item xs={6}>
-          <h2>{name}</h2>
-          <div className="reviews">
-            <Rating
-              className="revies_stars"
-              name="read-only"
-              value={avgRating}
-              readOnly
-            />
-            <p>
-              From <span className="reviews_number">{reviews.length}</span>{" "}
-              review{reviews.length === 1 ? "" : "s"}
-            </p>
-          </div>
-          <Link
-            className="btn"
-            to={{
-              pathname: `/product/${id}/add-review`,
-              state: {
-                imgUrl: imgUrl,
-                name: name,
-              },
-            }}
-          >
-            Review it
-          </Link>
-        </Grid>
-      </Grid>
-      {reviews.length > 0 && (
-        <Grid container spacing={2}>
-          <Grid item xs={12}>
-            <h3>Reviews</h3>
-            <ReviewList reviews={reviews} />
+    <div className="Product">
+      <Box sx={{ flexGrow: 1 }}>
+        <Grid className="product_header" container spacing={{ xs: 2, md: 3 }}>
+          <Grid item sm={12} md={6}>
+            <img className="product_img" src={imgUrl} alt={name} />
+          </Grid>
+          <Grid item sm={12} md={6}>
+            <h2>{name}</h2>
+            <div className="product_reviews">
+              <Rating
+                className="revies_stars"
+                name="read-only"
+                value={avgRating}
+                readOnly
+              />
+              <p className="product_avg">
+                From <span className="reviews_number">{reviews.length}</span>{" "}
+                review{reviews.length === 1 ? "" : "s"}
+              </p>
+            </div>
+            <Link
+              className="btn"
+              to={{
+                pathname: `/product/${id}/add-review`,
+                state: {
+                  imgUrl: imgUrl,
+                  name: name,
+                },
+              }}
+            >
+              Review it
+            </Link>
           </Grid>
         </Grid>
-      )}
-    </Box>
+        {reviews.length > 0 && (
+          <Grid className="product_reviews" container spacing={2}>
+            <Grid item xs={12}>
+              <h2>Reviews</h2>
+              <ReviewList reviews={reviews} />
+            </Grid>
+          </Grid>
+        )}
+      </Box>
+    </div>
   );
 }
 
--- src/Components/ReviewListItem.js
+++ src/Components/ReviewListItem.js
@@ -5,13 +5,24 @@ function ReviewListItem({ review }) {
   const { author, star_rating, headline, body } = review;
 
   return (
-    <li className="review">
-      <Avatar
-        alt={author}
-        src={`https://avatars.dicebear.com/api/human/${author}.svg`}
-      />
-      <h3>{author}</h3>
-      <Rating name="read-only" value={star_rating} readOnly />
+    <li className="ReviewListItem">
+      <div className="review_author">
+        <Avatar
+          className="author_avatar"
+          alt={author}
+          src={`https://avatars.dicebear.com/api/human/${author}.svg`}
+        />
+        <div className="right-wrapper">
+          <h3 className="author_name">{author}</h3>
+          <Rating
+            className="review_rating"
+            name="read-only"
+            value={star_rating}
+            readOnly
+          />
+        </div>
+      </div>
+
       <h4 className="review_headline">{headline}</h4>
       <p className="review_body">{body}</p>
     </li>
